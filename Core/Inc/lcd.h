1e1086c158c441f3fe2709fc40cf347815f3f72e
YashJ619 <yjasnani5@gmail.com>
2023-03-30 22:18:32 +0530

    Mx project created for LCD 16x4

---
+++ Core/Inc/lcd.h
@@ -0,0 +1,13 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Mar 30, 2023
+ *      Author: Yash
+ */
+
+#ifndef INC_LCD_H_
+#define INC_LCD_H_
+
+#include "stm32f1xx_hal.h"
+
+#endif /* INC_LCD_H_ */
---
+++ Core/Src/lcd.c
@@ -0,0 +1,8 @@
+/*
+ * lcd.c
+ *
+ *  Created on: Mar 30, 2023
+ *      Author: Yash
+ */
+
+#include "lcd.h"
